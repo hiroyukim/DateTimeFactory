ab2e8b3de21fca3c8efa6d390a5dd22bcfbccc26
hiroyuki <hiroyukimm@gmail.com>
2012-01-05 15:04:52 +0900

    setup

---
+++ lib/DateTimeFactory.pm
@@ -0,0 +1,34 @@
+package DateTimeFactory;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+use DateTime;
+
+1;
+__END__
+
+=head1 NAME
+
+DateTimeFactory -
+
+=head1 SYNOPSIS
+
+  use DateTimeFactory;
+
+=head1 DESCRIPTION
+
+DateTimeFactory is
+
+=head1 AUTHOR
+
+yamanaka hiroyuki E<lt>default {at} example.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
